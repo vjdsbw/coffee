ff49782459231758d4024175591740fbe0ff22c9
wangmanqing <1537957527@qq.com>
2024-07-05 22:21:56 +0800

    uid调试

--- src/views/NewIndex/index.vue
+++ src/views/NewIndex/index.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts" name="new-index">
 import lookStore from "@/assets/lookStore.png";
-import { nearestApi, productMenuApi } from '@/api/storeApi'
-import { purchasablePriceApi } from '@/api/productApi'
-import { productDetailApi, cartListApi } from "@/api/productApi";
-import { preCreateOrderApi, createOrderApi } from "@/api/orderApi";
-import { Store } from "@/store";
+import {nearestApi, productMenuApi} from '@/api/storeApi'
+import {cartListApi, productDetailApi, purchasablePriceApi} from '@/api/productApi'
+import {createOrderApi, preCreateOrderApi} from "@/api/orderApi";
+import {Store} from "@/store";
 import Cart from './components/cart.vue'
 import Product from './components/product.vue'
+
 const router = useRouter();
 
 const { global, order, user } = Store();
@@ -92,7 +92,7 @@ const getLimitPrice = async () => {
     if (data.status === 1) {
       limitPrice.value = data.couponPrice
       global.setLimitPrice(data)
-      success({ coords: { latitude: '32.086826', longitude: '118.795996' } })
+      // success({ coords: { latitude: '32.086826', longitude: '118.795996' } })
     }else{
       router.push({ name: 'Cart-paymentDetails' })
     }
@@ -117,15 +117,15 @@ onMounted(async () => {
   }
 
 
-  // if (navigator.geolocation) {
-  // 	navigator.geolocation.getCurrentPosition(success, error, {
-  // 		enableHighAccuracy: true,
-  // 		timeout: 5000,
-  // 		maximumAge: 0
-  // 	});
-  // } else {
-  // 	console.log("Geolocation is not supported by this browser.");
-  // }
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, error, {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    });
+  } else {
+    console.log("Geolocation is not supported by this browser.");
+  }
 
 });
 
